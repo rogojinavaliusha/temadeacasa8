8276b85b7bbb7e6a0a6ff85b22b76f37154c9b2e
Valentina Rogojina <rogojinavaliusha@gmail.com>
2023-09-17 19:43:53 +0300

    Final2

---
+++ Final2/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.Write("Введите значение M: ");
+
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите значение N: ");
+int n = int.Parse(Console.ReadLine());
+
+int sum = CalculateSum(m, n);
+
+Console.WriteLine($"Сумма натуральных чисел от {m} до {n} равна: {sum}");
+
+static int CalculateSum(int m, int n)
+{
+
+if (m > n)
+{
+    return 0;
+}
+
+return m + CalculateSum(m + 1, n);
+}
+
